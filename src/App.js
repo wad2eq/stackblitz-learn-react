eb6222ff57d2d3f8b890abfca52312aa2dcf98ea
wad2eq <mirekkedzierski@gmail.com>
2023-07-11 07:33:12 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from "react";
-import "./style.css";
+import React, { useState } from 'react';
+import './style.css';
 
 export default function App() {
+  const [data, setData] = useState(12);
+  let d = 12;
+  function incrementState() {
+    d += 12;
+    console.log(d);
+    const start = data + 1;
+    setData(start);
+  }
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+      <p>this is my state {data}</p>
+      <button onClick={incrementState}>Update state by one</button>
     </div>
   );
 }
